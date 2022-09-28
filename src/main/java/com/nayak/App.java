c02c28d4c806df0694346a39dea664488270dbb6
Sushil Nayak <sushil.nayak@gmail.com>
2022-09-29 00:16:34 +0530

    added extra !!

--- src/main/java/com/nayak/App.java
+++ src/main/java/com/nayak/App.java
@@ -2,6 +2,6 @@ package com.nayak;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!!");
+        System.out.println("Hello World!!!!");
     }
 }
